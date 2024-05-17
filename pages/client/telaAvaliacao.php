5feebaa548434092c0e58c2b74cb50221f9ffcda
rossinicostavinicius <cansadoelite@gmail.com>
2024-05-16 21:32:47 -0300

    create: telaAvaliacao, update: atualiazão dessa tela

--- pages/client/telaAvaliacao.php
+++ pages/client/telaAvaliacao.php
@@ -8,7 +8,7 @@
     <!-- Option 1: Include in HTML -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
     <title>Axey</title>
 </head>
 
@@ -55,7 +55,37 @@
         </nav>
     </header>
 
-    
+    <section class="avaliacao container-fluid py-4">
+        <h1 class="text-center">Avaliação</h1>
+        <div class="imagem container">
+            <img src="../../assets/img/icones_usuario/854888-200.png" alt="Ícone de usuário">
+        </div>
+        <div class="container">
+            <form action="index.php" method="post">
+                <div class="rate r">
+                    <input type="radio" id="star5" name="rate" value="5" />
+                    <label for="star5" title="text">5 stars</label>
+                    <input type="radio" id="star4" name="rate" value="4" />
+                    <label for="star4" title="text">4 stars</label>
+                    <input type="radio" id="star3" name="rate" value="3" />
+                    <label for="star3" title="text">3 stars</label>
+                    <input type="radio" id="star2" name="rate" value="2" />
+                    <label for="star2" title="text">2 stars</label>
+                    <input type="radio" id="star1" name="rate" value="1" />
+                    <label for="star1" title="text">1 star</label>
+                </div>
+                <div class="row mb-3 py-5">
+                    <label for="nome" class="col-sm-2 col-form-label">Nome</label>
+                    <textarea class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required></textarea>
+                </div>
+                <div class="row mb-3 py-5">
+                    <label for="mensagem" class="col-sm-2 col-form-label">Mensagem</label>
+                    <textarea class="form-control" id="mensagem" name="mensagem" placeholder="Digite sua mensagem" required></textarea>
+                </div>    
+            </form>
+        </div>
+    </section>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
