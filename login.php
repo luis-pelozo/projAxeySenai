355bb5486d24d7e8696b1b6a31bd87546b0140ed
luis-pelozo <pelozoluis52@gmail.com>
2024-05-16 21:53:36 -0300

    tela login ok (flta dividir em divs)

--- login.php
+++ login.php
@@ -1,44 +1,67 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .container-fluid{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh; /* Define a altura mínima da tela inteira */
+            margin: 0; /* Remova a margem padrão do corpo */
+        }
+        .cardLogin {
+            padding: 30px;
+            display: flex;
+            flex-direction: column; 
+            align-items: center;
+        }
+        .cardLogin input {
+            margin-bottom: 10px;
+            background-color: whitesmoke;
+        }
+        .btnEsqueciSenha {
+            padding: 4px;
+            margin-top: 10px;
+        }
+        .cardCadastre {
+            margin-top: 30px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 2%;
+        }
 
-    <head>
-    <meta charset="utf-8" />
-        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-        <title>Entrar</title>
+    </style>
+</head>
+<body>
+  <div class="container-fluid">
+  <div class="card col-md-4 cardLogin">
+        <img src="assets/imagens/logoAxey.png" class="card-img-top" alt="Imagem de Login">
+        <input type="text" class="form-control form-control-sm"  placeholder="Usuário">
+        <input type="password" class="form-control form-control-sm" placeholder="Senha">
+        <button type="submit" class="btn btn-primary btn-sm btn-block" style="color: white; border: none; background-color: #1A3C53;">Entrar</button>
+        <a href="#" class="btnEsqueciSenha btn-sm" style="color: #00376B;">Esqueci minha senha</a>
+       
+        <div class="card cardCadastre col-sm-10">
+            <span class="card-text">Não tem uma conta?
+                <a href="#" style="display: inline-block;"> Cadastre-se</a>
+            </span>
+        </div>
+    </div>  
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
-    </head>
-
-    <body>
-        <header>
-        </header>
-            <div class="container-fluid d-flex justify-content-center align-items-center vh-100"style="width: 18rem;">
-                <section id="login">
-                    <div class="card" id="imageLogin" style="width: 18rem;">
-                        <img src="assets/imagens/logoAxey.png" class="card-img-top" alt="...">
-                        <input type="text" placeholder="Usuário" name="usuarioLogin" id="usuarioLogin">
-                        <br>
-                        <input type="password" placeholder="Senha" name="senhaLogin" id="senhaLogin" >
-                        
-                        <input id="login-btn" type="submit" name="submit" value="Enviar">
-                    </div>
-                    
-                </section>
-            </div>
-        
-        <footer id="footer">
-            <div class="container">
-                <div class="copyright">
-                    &copy; <a href="#" target="_blank">axey</a>
-                </div>
-            </div>
-        </footer>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-            crossorigin="anonymous"></script>
-        <script src="../js/script.js"></script>
-    </body>
+    <footer style="position: absolute;bottom: 0;">
+        <p>&copy; axey</p>
+    </footer>
+  </div>
+</body>
 </html>
