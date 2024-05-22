22dbde9a3d5112f3dd02907cebab99e94e2521f2
rossinicostavinicius <cansadoelite@gmail.com>
2024-05-21 21:48:33 -0300

    update: tela avaliaçao atualizada

--- pages/client/telaAvaliacao.php
+++ pages/client/telaAvaliacao.php
@@ -4,89 +4,125 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <!-- Option 1: Include in HTML -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../assets/css/style.css">
-    <title>Axey</title>
+    <title>Avaliação</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        textarea {
+            resize: none;
+            min-width: 20vh;
+            height: auto;
+        }
+
+        .form-container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 50px;
+        }
+
+        .rate {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 20px;
+        }
+
+        .rate input[type="radio"] {
+            display: none;
+        }
+
+        .rate label {
+            font-size: 2rem;
+            color: grey;
+            cursor: pointer;
+            transition: color 0.3s;
+        }
+
+        .rate input[type="radio"]:checked~label,
+        .rate label:hover,
+        .rate label:hover~label {
+            color: gold;
+        }
+    </style>
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+    <div class="container d-flex justify-content-center">
+        <div class="form-container col-12 col-md-8 col-lg-6">
+            <h1 class="text-center py-2">Avaliação</h1>
+            <div class="text-center">
+                <img src="https://via.placeholder.com/150" alt="Ícone de usuário" class="mb-3">
             </div>
-        </nav>
-    </header>
-
-    <section class="avaliacao container-fluid py-4">
-        <h1 class="text-center">Avaliação</h1>
-        <div class="imagem container">
-            <img src="../../assets/img/icones_usuario/854888-200.png" alt="Ícone de usuário">
-        </div>
-        <div class="container">
             <form action="index.php" method="post">
-                <div class="rate r">
+                <div class="rate">
                     <input type="radio" id="star5" name="rate" value="5" />
-                    <label for="star5" title="text">5 stars</label>
+                    <label for="star5" title="5 estrelas">★</label>
                     <input type="radio" id="star4" name="rate" value="4" />
-                    <label for="star4" title="text">4 stars</label>
+                    <label for="star4" title="4 estrelas">★</label>
                     <input type="radio" id="star3" name="rate" value="3" />
-                    <label for="star3" title="text">3 stars</label>
+                    <label for="star3" title="3 estrelas">★</label>
                     <input type="radio" id="star2" name="rate" value="2" />
-                    <label for="star2" title="text">2 stars</label>
+                    <label for="star2" title="2 estrelas">★</label>
                     <input type="radio" id="star1" name="rate" value="1" />
-                    <label for="star1" title="text">1 star</label>
+                    <label for="star1" title="1 estrela">★</label>
                 </div>
-                <div class="row mb-3 py-5">
-                    <label for="nome" class="col-sm-2 col-form-label">Nome</label>
-                    <textarea class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required></textarea>
+                <div class="form-group">
+                    <label>Como você classifica a pontualidade do prestador</label>
+                    <div class="d-flex justify-content-around">
+                        <div class="op1">
+                            <input type="radio" id="pontualidadeBoa" name="pontualidade" value="Boa" />
+                            <label for="pontualidadeBoa">Boa</label>
+                        </div>
+                        <div class="op2">
+                            <input type="radio" id="pontualidadeMediana" name="pontualidade" value="Mediana" />
+                            <label for="pontualidadeMediana">Mediana</label>
+                        </div>
+                        <div class="op3">
+                            <input type="radio" id="pontualidadeRuim" name="pontualidade" value="Ruim" />
+                            <label for="pontualidadeRuim">Ruim</label>
+                        </div>
+                    </div>
                 </div>
-                <div class="row mb-3 py-5">
-                    <label for="mensagem" class="col-sm-2 col-form-label">Mensagem</label>
-                    <textarea class="form-control" id="mensagem" name="mensagem" placeholder="Digite sua mensagem" required></textarea>
-                </div>    
+                <div class="form-group">
+                    <label>Aspectos da Satisfação (Marque todos os aplicáveis):</label>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="aspecto1" name="aspectos[]" value="Cortesia">
+                        <label class="form-check-label" for="aspecto1">Cortesia</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="aspecto2" name="aspectos[]" value="Profissionalismo">
+                        <label class="form-check-label" for="aspecto2">Profissionalismo</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="aspecto3" name="aspectos[]" value="Qualidade do Serviço">
+                        <label class="form-check-label" for="aspecto3">Qualidade do Serviço</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="aspecto4" name="aspectos[]" value="Tempo de Resolução">
+                        <label class="form-check-label" for="aspecto4">Tempo de Resolução</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="aspecto5" name="aspectos[]" value="Preço Justo">
+                        <label class="form-check-label" for="aspecto5">Preço Justo</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="mensagem">Observação</label>
+                    <textarea class="form-control" id="mensagem" name="mensagem" placeholder="Digite sua mensagem" rows="4" required></textarea>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn text-light" style="background-color: #1B3C54; width: 57%;">Enviar</button>
+                </div>
             </form>
         </div>
-    </section>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
