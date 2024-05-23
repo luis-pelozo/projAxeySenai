d11a3b3f64d662dc4d5310fdd76cee9e63d3be39
rossinicostavinicius <cansadoelite@gmail.com>
2024-05-22 21:30:28 -0300

    update

--- pages/prestador/telaCadastroProduto.php
+++ pages/prestador/telaCadastroProduto.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Avaliação</title>
+    <title>Cadastro de Serviço / Produto</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <style>
         body {
             background-color: #f8f9fa;
@@ -13,8 +15,6 @@
 
         textarea {
             resize: none;
-            min-width: 20vh;
-            height: auto;
         }
 
         .form-container {
@@ -25,102 +25,106 @@
             margin-top: 50px;
         }
 
-        .rate {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 20px;
+        .form-container h1 {
+            color: #1B3C54;
         }
 
-        .rate input[type="radio"] {
-            display: none;
+        .preview img {
+            max-width: 100px;
+            margin-top: 10px;
         }
 
-        .rate label {
-            font-size: 2rem;
-            color: grey;
-            cursor: pointer;
-            transition: color 0.3s;
-        }
-
-        .rate input[type="radio"]:checked~label,
-        .rate label:hover,
-        .rate label:hover~label {
-            color: gold;
+        .char-counter {
+            color: #6c757d;
+            font-size: 0.875rem;
         }
     </style>
 </head>
 
 <body>
     <div class="container d-flex justify-content-center">
-        <div class="form-container col-12 col-md-8 col-lg-6">
-            <h1 class="text-center py-2">Avaliação</h1>
-            <div class="text-center">
-                <img src="https://via.placeholder.com/150" alt="Ícone de usuário" class="mb-3">
-            </div>
-            <form action="index.php" method="post">
-                <div class="rate">
-                    <input type="radio" id="star5" name="rate" value="5" />
-                    <label for="star5" title="5 estrelas">★</label>
-                    <input type="radio" id="star4" name="rate" value="4" />
-                    <label for="star4" title="4 estrelas">★</label>
-                    <input type="radio" id="star3" name="rate" value="3" />
-                    <label for="star3" title="3 estrelas">★</label>
-                    <input type="radio" id="star2" name="rate" value="2" />
-                    <label for="star2" title="2 estrelas">★</label>
-                    <input type="radio" id="star1" name="rate" value="1" />
-                    <label for="star1" title="1 estrela">★</label>
+        <div class="form-container col-12 col-md-10 col-lg-8">
+            <h1 class="text-center py-2">Cadastro de Serviço / Produto</h1>
+            <form action="save_service.php" method="post" enctype="multipart/form-data">
+                <div class="form-row">
+                    <div class="form-group col-md-8">
+                        <label for="serviceName">Titulo</label>
+                        <input type="text" class="form-control" id="serviceName" name="serviceName" required>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="serviceCategory">Seguimento</label>
+                        <select class="form-control" id="serviceCategory" name="serviceCategory" required>
+                            <option value="" disabled selected>Selecione uma categoria</option>
+                            <option value="Consultoria">Consultoria</option>
+                            <option value="Treinamento">Treinamento</option>
+                            <option value="Manutenção">Manutenção</option>
+                            <option value="Desenvolvimento">Desenvolvimento</option>
+                            <option value="Outros">Outros</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label>Como você classifica a pontualidade do prestador</label>
-                    <div class="d-flex justify-content-around">
-                        <div class="op1">
-                            <input type="radio" id="pontualidadeBoa" name="pontualidade" value="Boa" />
-                            <label for="pontualidadeBoa">Boa</label>
-                        </div>
-                        <div class="op2">
-                            <input type="radio" id="pontualidadeMediana" name="pontualidade" value="Mediana" />
-                            <label for="pontualidadeMediana">Mediana</label>
-                        </div>
-                        <div class="op3">
-                            <input type="radio" id="pontualidadeRuim" name="pontualidade" value="Ruim" />
-                            <label for="pontualidadeRuim">Ruim</label>
-                        </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="serviceDuration">Duração (horas / dias)</label>
+                        <input type="number" class="form-control" id="serviceDuration" name="serviceDuration" step="0.1" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="servicePrice">Valor</label>
+                        <input type="text" class="form-control" id="servicePrice" name="servicePrice" maxlength="42" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label>Aspectos da Satisfação (Marque todos os aplicáveis):</label>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="aspecto1" name="aspectos[]" value="Cortesia">
-                        <label class="form-check-label" for="aspecto1">Cortesia</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="aspecto2" name="aspectos[]" value="Profissionalismo">
-                        <label class="form-check-label" for="aspecto2">Profissionalismo</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="aspecto3" name="aspectos[]" value="Qualidade do Serviço">
-                        <label class="form-check-label" for="aspecto3">Qualidade do Serviço</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="aspecto4" name="aspectos[]" value="Tempo de Resolução">
-                        <label class="form-check-label" for="aspecto4">Tempo de Resolução</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="aspecto5" name="aspectos[]" value="Preço Justo">
-                        <label class="form-check-label" for="aspecto5">Preço Justo</label>
-                    </div>
+                    <label for="serviceDescription">Descrição</label>
+                    <textarea class="form-control" id="serviceDescription" name="serviceDescription" rows="4" maxlength="900" required></textarea>
+                    <small class="char-counter"><span id="charCount">0</span>/900 caracteres</small>
                 </div>
                 <div class="form-group">
-                    <label for="mensagem">Observação</label>
-                    <textarea class="form-control" id="mensagem" name="mensagem" placeholder="Digite sua mensagem" rows="4" required></textarea>
+                    <label for="serviceImages">Imagens</label>
+                    <input type="file" class="form-control-file" id="serviceImages" name="serviceImages[]" multiple accept="image/*" onchange="previewImages()">
+                    <div id="imagePreview" class="preview d-flex flex-wrap"></div>
+                </div>
+                <div class="form-group">
+                    <label for="serviceVideos">Vídeos</label>
+                    <input type="file" class="form-control-file" id="serviceVideos" name="serviceVideos[]" multiple accept="video/*">
                 </div>
                 <div class="text-center">
-                    <button type="submit" class="btn text-light" style="background-color: #1B3C54; width: 57%;">Enviar</button>
+                    <button type="submit" class="btn text-light" style="background-color: #1B3C54; width: 57%;">Cadastrar</button>
                 </div>
             </form>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            $('#servicePrice').mask('#.##0,00', {reverse: true});
+
+            $('#serviceDescription').keyup(function() {
+                var charCount = $(this).val().length;
+                $('#charCount').text(charCount);
+            });
+        });
+
+        function previewImages() {
+            var preview = document.getElementById('imagePreview');
+            preview.innerHTML = '';
+            var files = document.getElementById('serviceImages').files;
+
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                var reader = new FileReader();
+
+                reader.onload = (function(e) {
+                    var img = document.createElement('img');
+                    img.src = e.target.result;
+                    img.classList.add('m-2');
+                    preview.appendChild(img);
+                });
+
+                reader.readAsDataURL(file);
+            }
+        }
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
