5396afab0ed049d9884cc83db441dd1ca5f7d718
rossinicostavinicius <cansadoelite@gmail.com>
2024-05-29 19:22:40 -0300

    update: limpando o que ficou pos merge

--- padroes/footer.php
+++ padroes/footer.php
@@ -1,4 +1,3 @@
-
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -7,40 +6,40 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 
-<style> 
- .bg-footer {
-            background-color: #012640;
-        }
+<style>
+    .bg-footer {
+        background-color: #012640;
+    }
 </style>
 <footer class="bg-footer text-white mt-5">
-        <div class="container py-4">
-            <div class="row">
-                <div class="col-md-4">
-                    <h5>Sobre Nós</h5>
-                    <p>Aqui você encontra diversas variedades de serviços para seu dia a dia.</p>
-                </div>
-                <div class="col-md-4">
-                    <h5>Links Úteis</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#" class="text-white">Início</a></li>
-                        <li><a href="#" class="text-white">Planos</a></li>
-                        <li><a href="#" class="text-white">Contato</a></li>
-                        <li><a href="#" class="text-white">Termos de Uso</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h5>Redes Sociais</h5>
-                    <a href="#" class="text-white mr-2"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="text-white mr-2"><i class="fab fa-twitter"></i></a>
-                    <a href="#" class="text-white mr-2"><i class="fab fa-instagram"></i></a>
-                    <a href="#" class="text-white mr-2"><i class="fab fa-linkedin"></i></a>
-                </div>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Sobre Nós</h5>
+                <p>Aqui você encontra diversas variedades de serviços para seu dia a dia.</p>
             </div>
-            <div class="text-center mt-3">
-                <p>&copy; 2024 Axey. Todos os direitos reservados.</p>
+            <div class="col-md-4">
+                <h5>Links Úteis</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Início</a></li>
+                    <li><a href="#" class="text-white">Planos</a></li>
+                    <li><a href="#" class="text-white">Contato</a></li>
+                    <li><a href="#" class="text-white">Termos de Uso</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Redes Sociais</h5>
+                <a href="#" class="text-white mr-2"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="text-white mr-2"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="text-white mr-2"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="text-white mr-2"><i class="fab fa-linkedin"></i></a>
             </div>
         </div>
-    </footer>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <div class="text-center mt-3">
+            <p>&copy; 2024 Axey. Todos os direitos reservados.</p>
+        </div>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
