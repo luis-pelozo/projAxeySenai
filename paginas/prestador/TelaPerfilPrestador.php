8fdca8fa766e6cb07e0dd4a162055158b8abe54b
rossinicostavinicius <cansadoelite@gmail.com>
2024-06-04 21:30:16 -0300

    update

--- paginas/prestador/TelaPerfilPrestador.php
+++ paginas/prestador/TelaPerfilPrestador.php
@@ -455,7 +455,7 @@
 
 
         document.getElementById('whatsappButton').addEventListener('click', function() {
-            const phoneNumber = '5547996522605'; // Número de telefone com código do país (55 para Brasil)
+            const phoneNumber = '554788671192'; // Número de telefone com código do país (55 para Brasil)
             const message = encodeURIComponent('Olá, gostaria de mais informações.'); // Mensagem opcional
             const url = `https://wa.me/${phoneNumber}?text=${message}`;
             window.open(url, '_blank');
