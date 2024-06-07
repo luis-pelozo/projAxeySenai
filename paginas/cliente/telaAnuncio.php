316aafa0c74baede8f2d2d5272a6b2987f69771e
rossinicostavinicius <cansadoelite@gmail.com>
2024-06-06 21:47:28 -0300

    update: placeholder para iamgens, caso o usuario nao tenha uma imagem

--- paginas/cliente/telaAnuncio.php
+++ paginas/cliente/telaAnuncio.php
@@ -1,27 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Axey</title>
 </head>
-
 <body>
-
-    <?php
-    include '../../padroes/nav.php';
-    ?>
+    <?php include '../../padroes/nav.php'; ?>
 
     <section class="anuncipo mx-5 py-5">
-        <div class="main container d-flex">
+        <div class="main container d-flex flex-column flex-md-row">
             <div id="separa-divs">
                 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner rounded-4">
@@ -32,7 +25,7 @@
                             <img src="../../assets/imgs/imgTeste.png" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="../../assets/imgs /imgTeste.png" class="d-block w-100" alt="...">
+                            <img src="../../assets/imgs/imgTeste.png" class="d-block w-100" alt="...">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -47,7 +40,7 @@
             </div>
             <div class="main-group-func container flex-wrap object-fit d-flex align-self-center">
                 <div class="icon img-fluid container altera-img">
-                    <img src="../../assets/img/icones_usuario/854888-200.png" alt="" class="rounded-circle">
+                    <img src="../../assets/imgs/user.png" alt="" class="rounded-circle">
                 </div>
                 <div class="legenda container text-center">
                     <p>illum quae eligendi unde ipsa reiciendis dolor assumenda voluptates recusandae animi nesciunt earum laboriosam.</p>
@@ -99,11 +92,8 @@
         </div>
     </section>
 
-    <?php
-    include '../../padroes/footer.php';
-    ?>
+    <?php include '../../padroes/footer.php'; ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </body>
-
 </html>
