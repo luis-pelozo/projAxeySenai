9a1c8c72ce81791d1b74c8aa57744b0806dac55a
AffonsoDavi <affonsodavi@gmail.com>
2024-06-06 21:04:06 -0300

    ok

--- paginas/cliente/telaAnuncio.php
+++ paginas/cliente/telaAnuncio.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/projAxeySenai/assets/css/anuncio.css">
     <title>Axey</title>
 </head>
 
@@ -19,8 +18,8 @@
     ?>
 
     <section class="anuncipo mx-5 py-5">
-        <div class="main container d-flex">
-            <div id="separa-divs">
+        <div class="main container d-flex flex-wrap">
+            <div id="separa-divs" class="col-md-8 mb-4">
                 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner rounded-4">
                         <div class="carousel-item active">
@@ -30,7 +29,7 @@
                             <img src="../../assets/imgs/imgTeste.png" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="../../assets/imgs /imgTeste.png" class="d-block w-100" alt="...">
+                            <img src="../../assets/imgs/imgTeste.png" class="d-block w-100" alt="...">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -43,61 +42,78 @@
                     </button>
                 </div>
             </div>
-            <div class="main-group-func container flex-wrap object-fit d-flex align-self-center">
-                <div class="icon img-fluid container altera-img">
+            <div class="main-group-func container flex-wrap d-flex align-self-center col-md-4">
+                <div class="icon img-fluid container altera-img text-center mb-3">
                     <img src="../../assets/img/icones_usuario/854888-200.png" alt="" class="rounded-circle">
                 </div>
-                <div class="legenda container text-center">
+                <div class="legenda container text-center mb-3">
                     <p>illum quae eligendi unde ipsa reiciendis dolor assumenda voluptates recusandae animi nesciunt earum laboriosam.</p>
                 </div>
-                <div class="buttom-gourp d-flex flex-column container">
-                    <a type="submit" class="btn btn-success"><span></span>Entre em contato</a>
-                    <div class="group-buttom d-flex flex-column py-2">
-                        <a type="submit" class="btn btn-primary">Verificar disponibilidade</a>
-                    </div>
+                <div class="buttom-gourp d-flex flex-column container text-center">
+                    <a type="submit" class="btn btn-success mb-2">Entre em contato</a>
+                    <a type="submit" class="btn btn-primary">Verificar disponibilidade</a>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="serviços-semelhantes">
-        <div class="container">
-            <h3>Serviços Semelhantes</h3>
-            <div class="d-flex">
-                <div class="row row-cols-1 row-cols-md-3 g-4">
-                    <div class="col">
-                        <div class="card">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
+    <div class="services text-center">
+        <h1>Serviços em destaque</h1>
+    </div>
+    <div class="container-fluid categories">
+    <div class="services mt-4 mx-0">
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Serviço 1</h5>
+                    <p class="card-text">Descrição breve do Serviço 1.</p>
+                    <a href="#" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Serviço 2</h5>
+                    <p class="card-text">Descrição breve do Serviço 2.</p>
+                    <a href="#" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Serviço 3</h5>
+                    <p class="card-text">Descrição breve do Serviço 3.</p>
+                    <a href="#" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Serviço 4</h5>
+                    <p class="card-text">Descrição breve do Serviço 4.</p>
+                    <a href="#" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Serviço 5</h5>
+                    <p class="card-text">Descrição breve do Serviço 5.</p>
+                    <a href="#" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Serviço 6</h5>
+                    <p class="card-text">Descrição breve do Serviço 6.</p>
+                    <a href="#" class="btn btn-primary">Saiba mais</a>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 
-    <?php 
+    <?php
     include '../../padroes/footer.php';
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
