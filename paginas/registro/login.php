e7c32c63be4a4187e20fc3a93f6bafa61deda595
AffonsoDavi <affonsodavi@gmail.com>
2024-06-11 19:59:36 -0300

    commit 2

--- paginas/registro/login.php
+++ paginas/registro/login.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
     include '../../padroes/head.php';
     
@@ -49,10 +52,10 @@
                     </span>
                 </div>
             </div>
-        </div>    
-        <?php
-        include '../../padroes/footer.php';
-        ?>
+        </div>      
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <script>
             document.getElementById('entrarBtn').addEventListener('click', function() {
                 window.location.href = '#';
